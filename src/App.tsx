a21e1de600cca64120e07de5e0a36aefda1037d7
Macarthur-yaw <arthurkevin1260@gmail.com>
2024-03-31 15:05:05 +0100

    removed padding

--- src/App.tsx
+++ src/App.tsx
@@ -27,11 +27,11 @@ function App() {
     selection:bg-teal-300  selection:text-teal-900 `}>
       <div className="font-sans
  
-   md:px-12 md:mx-auto min-h-screen md:max-w-screen-xl px-6 py-12 md:py-20 lg:px-24 lg:py-0">
+   md:px-12 md:mx-auto min-h-screen md:max-w-screen-xl px-2 py-12 md:py-20 lg:px-24 lg:py-0">
    <div className="">
     <SideNavBar changeTheme={changeTheme}/>
     </div> 
-    <div className=" md:w-[40%] md:ml-auto p-4 pt-24 lg:w-1/2 lg:py-24 ">
+    <div className=" md:w-[40%] md:ml-auto p-4 md:pt-24 lg:w-1/2 lg:py-24 ">
     <About />
       <Experience/>
       <Projects/>
--- src/Components/SideNavBar.tsx
+++ src/Components/SideNavBar.tsx
@@ -5,7 +5,7 @@ type propData={
 }
 const SideNavBar = ({changeTheme}:propData) => {
 return ( 
-      <nav className=" lg:top-0 lg:flex lg:max-h-screen lg:flex-col lg:justify-between lg:py-24 md:h-screen  p-10 md:w-[40%]  md:fixed">
+      <nav className=" lg:top-0 lg:flex lg:max-h-screen lg:flex-col p-4 lg:justify-between lg:py-24 md:h-screen  md:p-10 md:w-[40%]  md:fixed">
             
             <div id="header">
        <span className="flex flex-row items-center justify-between">
@@ -21,14 +21,14 @@ return (
                     Frontend Engineer
                 </h2>
 
-                <h3 className="mt-4 mx-w-xs leading-normal text-[16px] text-[#94A3BB]">
+                <h3 className="mt-4 md:mx-w-xs leading-normal text-[16px] text-[#94A3BB]">
                 I create digital experiences that are meticulously crafted, captivating, and easily accessible
 
                 </h3>
             </div>
 
             <div id="main-nav" className="lg:block hidden">
-<ul className="mt-16 w-max list-none ">
+<ul className="mt-16 md:w-max list-none ">
 <li><a className="group flex items-center py-3 active" href="#about"><span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span><span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">About</span></a></li>
 
 <li><a className="group flex items-center py-3 active" href="#experience"><span className="nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span><span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200">Experience</span></a></li>
